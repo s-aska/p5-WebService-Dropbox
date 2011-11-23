5a8e00cad98ebf7bcc8b398470b23b945c3c5f49
Shinichiro Aska <s.aska.org@gmail.com>
2011-11-23 21:10:53 +0900

    initial commit

---
+++ lib/WebService/Dropbox.pm
@@ -0,0 +1,32 @@
+package WebService::Dropbox;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Dropbox -
+
+=head1 SYNOPSIS
+
+  use WebService::Dropbox;
+
+=head1 DESCRIPTION
+
+WebService::Dropbox is
+
+=head1 AUTHOR
+
+Shinichiro Aska E<lt>s.aska.org {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ xt/01_podspell.t
@@ -0,0 +1,10 @@
+use Test::More;
+eval q{ use Test::Spelling };
+plan skip_all => "Test::Spelling is not installed." if $@;
+add_stopwords(map { split /[\s\:\-]/ } <DATA>);
+$ENV{LANG} = 'C';
+all_pod_files_spelling_ok('lib');
+__DATA__
+Shinichiro Aska
+s.aska.org {at} gmail.com
+WebService::Dropbox
